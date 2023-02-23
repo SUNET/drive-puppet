d378b5f2dab8471377468f3e6f46a01dc2231c52
Micke Nordin <kano@sunet.se>
2023-02-23 16:07:01 +0100

    Skip over statistics

--- templates/script/collect_backup_data.erb.sh
+++ templates/script/collect_backup_data.erb.sh
@@ -3,7 +3,7 @@
 project="${1}"
 bucket="${2}"
 data_dir='/opt/backups/data'
-for project in $(rclone listremotes | grep -v 'mirror'); do
+for project in $(rclone listremotes | grep -v 'mirror' | grep -v 'statistics'); do
 	for bucket in $(rclone lsd "${project}" | awk '{print $NF}' | grep -E '\-mirror|db-backups'); do
     mkdir -p "${data_dir}/${project}" 
 		duplicity collection-status --log-file /dev/stdout --no-encryption "rclone://${project}${bucket}" | grep -E '^ inc|^ full' > "${data_dir}/${project}/${bucket}.dat"
