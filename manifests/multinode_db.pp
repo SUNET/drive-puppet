b62746ac66d969d7fd4f9f993702ea6e8989ce5a
Micke Nordin <kano@sunet.se>
2023-02-27 10:14:26 +0100

    Add multinode_db class

---
+++ manifests/multinode_db.pp
@@ -0,0 +1,17 @@
+class sunetdrive::multinode_db(){
+    $is_multinode = true;
+    $environment = sunetdrive::get_environment()
+    $allcustomers = hiera_hash('multinode_mapping')
+    $customers = $allcustomers.keys
+
+    $customers.each | $customer | do {
+      file { "/etc/mariadb/backups/${customer}":
+        ensure => directory,
+      }
+      file { "/etc/mariadb/init/04-nextcloud.${customer}.sql":
+        ensure  => present,
+        content => "CREATE SCHEMA nextcloud_${customer};\nCREATE USER 'nextcloud_${customer}'@'%' IDENTIFIED BY '${hiera("${customer}_mysql_user_password")}';\nGRANT ALL PRIVILEGES ON nextcloud_${customer}.* TO 'nextcloud_${customer}'@'%' IDENTIFIED BY '${hiera("${customer}_mysql_user_password")}';\n",
+        mode    => '0744',
+      }
+    }
+}
