8386df91de1dd72c80533ebf6c8bbe09552e7291
Micke Nordin <kano@sunet.se>
2023-03-24 14:36:07 +0100

    Fix for multinode

--- templates/proxysql/insert_server_in_proxysql.erb.sh
+++ templates/proxysql/insert_server_in_proxysql.erb.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 PATH="${PATH}:/usr/local/bin"
 restarted="false"
+domain=$(hostname -d)
+prefix="intern-db"
+if [[ ${domain} =~ ^drive ]]; then
+  prefix="multinode-db"
+fi
+
 for index in 1 2 3; do
-	db_ip=$(host "intern-db${index}.$(hostname -d)" | awk '/has address/ {print $NF}')
+	db_ip=$(host "${prefix}${index}.${domain}" | awk '/has address/ {print $NF}')
   result=$(proxysql "select * from main.mysql_servers where hostname = '${db_ip}' and hostgroup_id = 10")
   if [[ -z ${result} ]]; then
     query="INSERT INTO main.mysql_servers (hostgroup_id, hostname, max_connections, comment) VALUES( 10, '${db_ip}', 100, 'Inserted by script at $(date)')"
