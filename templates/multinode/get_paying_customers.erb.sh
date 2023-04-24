c5e5279f0e083b6ed3ea78b9135d0bf5f6fb8f02
Micke Nordin <kano@sunet.se>
2023-04-24 12:10:30 +0200

    Add reference to self

--- templates/multinode/get_paying_customers.erb.sh
+++ templates/multinode/get_paying_customers.erb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+me="$(hostname -s)"
 cat /etc/hiera/data/common.yaml | yq -r '.multinode_mapping| to_entries |map({name: .key} + .value)| map(select(.server == "'"${me}"'")) |.[] |.name' |
 	grep -E "$(cat /etc/hiera/data/common.yaml | yq -r '.singlenodes[]' | sed -e 's/^- //' -e 's/$/|/' | tr -d '\n' | sed 's/|$//')"
