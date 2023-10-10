88ee8c2b672e584af28efdea654fc000b76feb58
Micke Nordin <kano@sunet.se>
2023-10-10 10:26:43 +0200

    Use collabora enterprise image

--- manifests/onlyoffice.pp
+++ manifests/onlyoffice.pp
@@ -3,6 +3,8 @@ class sunetdrive::onlyoffice () {
   $environment = sunetdrive::get_environment()
   $extra_hosts = hiera_hash($environment)['extra_hosts']
   $docker_tag  = hiera_hash($environment)['collabora_tag']
+  $token_name = 'Sunet'
+  $access_token = safe_hiera('collabora_access_token')
   $customers = hiera('fullnodes')
   $multinode_customers = keys(hiera_hash('multinode_mapping'))
   if $environment == 'prod' {
@@ -10,11 +12,15 @@ class sunetdrive::onlyoffice () {
   }  else {
     $domain = 'drive.test.sunet.se'
   }
+  exec { 'collabora_docker_login':
+    cmd => "docker login registry.gitlab.collabora.com -u ${token_name} -p ${access_token}",
+  }
   sunet::collabora::docs { 'sunet-onlyoffice':
     dns         => [ '89.32.32.32' ],
     extra_hosts => $extra_hosts,
     extra_volumes => ['/opt/collabora/coolwsd.xml:/etc/coolwsd/coolwsd.xml'],
     docker_tag  => $docker_tag,
+    docker_image => 'registry.gitlab.collabora.com/productivity/collabora-online-nc',
   }
   file {'/opt/collabora/coolwsd.xml':
     ensure => present,
